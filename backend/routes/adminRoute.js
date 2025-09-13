c4a0141bb542ee32d290f3133b319ece14c1cd8f
sant1ag0Rr <drodriguez442@soyudemedellin.edu.co>
2025-09-13 11:11:30 -0500

    imported multiple times

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,35 +1,70 @@
-import express from "express"
-import { adminAuth ,adminProfiile } from "../controllers/adminControllers/adminController.js"
-import { signIn } from "../controllers/authController.js"
-import { signOut  } from "../controllers/userControllers/userController.js"
-import { addProduct, deleteVehicle, editVehicle,  } from "../controllers/adminControllers/dashboardController.js"
-import { showVehicles } from "../controllers/adminControllers/dashboardController.js"
-import { multerUploads } from "../utils/multer.js"
-import { insertDummyData } from "../controllers/adminControllers/masterCollectionController.js"
-import { getCarModelData } from "../controllers/adminControllers/masterCollectionController.js"
-import { approveVendorVehicleRequest, fetchVendorVehilceRequests, rejectVendorVehicleRequest } from "../controllers/adminControllers/vendorVehilceRequests.js"
-import { allBookings, changeStatus } from "../controllers/adminControllers/bookingsController.js"
-import { verifyToken } from "../utils/verifyUser.js"
+import express from "express";
+
+// Authentication and middleware imports
+import { signIn } from "../controllers/authController.js";
+import { signOut } from "../controllers/userControllers/userController.js";
+import { verifyToken } from "../utils/verifyUser.js";
+import { multerUploads } from "../utils/multer.js";
+
+// Admin controller imports
+import { adminAuth, adminProfiile } from "../controllers/adminControllers/adminController.js";
 import { getAllUsers, getDashboardStats } from '../controllers/adminController.js';
 
+// Dashboard controller imports (unified - no more duplicates)
+import { 
+  addProduct, 
+  deleteVehicle, 
+  editVehicle, 
+  showVehicles 
+} from "../controllers/adminControllers/dashboardController.js";
 
-const router = express.Router()
+// Master collection controller imports (unified)
+import { 
+  insertDummyData, 
+  getCarModelData 
+} from "../controllers/adminControllers/masterCollectionController.js";
 
-router.post('/dashboard',signIn,adminAuth)
-router.post('/profile',adminProfiile)
-router.get('/signout',signOut)
-router.post('/addProduct',multerUploads,addProduct)
-router.get('/showVehicles',showVehicles)
-router.delete('/deleteVehicle/:id',deleteVehicle)
-router.put('/editVehicle/:id',editVehicle)
-router.get('/dummyData',insertDummyData)
-router.get('/getVehicleModels',getCarModelData)
-router.get('/fetchVendorVehilceRequests',fetchVendorVehilceRequests)
-router.post('/approveVendorVehicleRequest',approveVendorVehicleRequest)
-router.post('/rejectVendorVehicleRequest',rejectVendorVehicleRequest)
-router.get('/allBookings',allBookings)
-router.post('/changeStatus',changeStatus)
+// Vendor vehicle requests imports
+import { 
+  approveVendorVehicleRequest, 
+  fetchVendorVehilceRequests, 
+  rejectVendorVehicleRequest 
+} from "../controllers/adminControllers/vendorVehilceRequests.js";
+
+// Bookings controller imports
+import { 
+  allBookings, 
+  changeStatus 
+} from "../controllers/adminControllers/bookingsController.js";
+
+const router = express.Router();
+
+// Authentication routes
+router.post('/dashboard', signIn, adminAuth);
+router.post('/profile', adminProfiile);
+router.get('/signout', signOut);
+
+// Vehicle management routes
+router.post('/addProduct', multerUploads, addProduct);
+router.get('/showVehicles', showVehicles);
+router.delete('/deleteVehicle/:id', deleteVehicle);
+router.put('/editVehicle/:id', editVehicle);
+
+// Master data routes
+router.get('/dummyData', insertDummyData);
+router.get('/getVehicleModels', getCarModelData);
+
+// Vendor vehicle request routes
+router.get('/fetchVendorVehilceRequests', fetchVendorVehilceRequests);
+router.post('/approveVendorVehicleRequest', approveVendorVehicleRequest);
+router.post('/rejectVendorVehicleRequest', rejectVendorVehicleRequest);
+
+// Booking management routes
+router.get('/allBookings', allBookings);
+router.post('/changeStatus', changeStatus);
+
+// User management routes
 router.get('/getAllUsers', getAllUsers);
 router.get('/getDashboardStats', getDashboardStats);
 
-export default router
+export default router;
