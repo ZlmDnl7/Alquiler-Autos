898cc57dbf7ee6fecd96c7359c8b53ed62e1cf79
Andres2004 <aarteaga731@soyudemedellin.edu.co>
2025-09-14 17:16:43 -0500

    Complete the task associated to this TODO comment.

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "@firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
+import { getAuth } from "@firebase/auth";
+import { getFirestore } from "@firebase/firestore";
+import { getStorage } from "@firebase/storage";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
@@ -15,3 +17,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
